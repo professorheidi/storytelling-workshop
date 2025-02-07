7a3e6b87b15b83d91fe10c74fd00fe94abb2fb17
professorheidi <heidi_schultz@unc.edu>
2025-02-07 10:34:59 -0500

    Delete components/ui directory

--- components/ui/input.js
+++
@@ -1,8 +0,0 @@
-export function Input(props) {
-  return (
-    <input
-      {...props}
-      className="px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-    />
-  );
-}
--- components/ui/textarea.js
+++
@@ -1,8 +0,0 @@
-export function Textarea(props) {
-  return (
-    <textarea
-      {...props}
-      className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-    />
-  );
-}
